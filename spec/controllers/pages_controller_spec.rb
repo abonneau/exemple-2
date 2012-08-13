b87afad6f458d55b60bc879f11349dc256cf22b2
abonneau <aurelienbonneau@free.fr>
2012-08-13 19:41:34 +0200

    ajout de la page help

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -6,36 +6,64 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+  before(:each) do
+    #definition de la base du titre
+    @base_title = "Simple App du Tutoriel Ruby on Rails"
+  end
+
+  #tests de la page "home"
   describe "GET 'home'" do
+    #test d'existence
     it "devrait réussir" do
       get 'home'
       response.should be_success
     end
+    #test de titre
     it "devrait avoir le bon titre" do
       get 'home'
-      response.should have_selector("title", :content => "Simple App du Tutoriel Ruby on Rails | Accueil")
+      response.should have_selector("title", :content => @base_title + " | Accueil")
     end
   end
 
+  #tests de la page "contact"
   describe "GET 'contact'" do
+    #test d'existence
     it "devrait réussir" do
       get 'contact'
       response.should be_success
     end
+    #test de titre
     it "devrait avoir le bon titre" do
       get 'contact'
-      response.should have_selector("title", :content => "Simple App du Tutoriel Ruby on Rails | Contact")
+      response.should have_selector("title", :content => @base_title + " | Contact")
     end
   end
 
+  #tests de la page "about"
   describe "GET 'about'" do
+    #test d'existence
     it "devrait réussir" do
       get 'about'
       response.should be_success
     end
+    #test de titre
     it "devrait avoir le bon titre" do
       get 'about'
-      response.should have_selector("title", :content => "Simple App du Tutoriel Ruby on Rails | A propos")
+      response.should have_selector("title", :content => @base_title + " | A propos")
+    end
+  end
+
+  #tests de la page "help"
+  describe "GET 'help'" do
+    #test d'existence
+    it "devrait réussir" do
+      get 'help'
+      response.should be_success
+    end
+    #test de titre
+    it "devrait avoir le bon titre" do
+      get 'help'
+      response.should have_selector("title", :content => @base_title + " | Aide")
     end
   end
 
